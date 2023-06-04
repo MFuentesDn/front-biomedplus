d999bc4467e735f06daa2dbe7989502295a6e81c
MFuentesDn <dayana.fuentesduran@gmail.com>
2023-06-04 15:02:25 -0500

    agregue la pestaña repuestos, donde se agrego repuestos.css y se edito repuestos.js creado inicalmente

--- src/pages/inventario/repuestos.js
+++ src/pages/inventario/repuestos.js
@@ -1,9 +1,56 @@
-
+import './repuestos.css';
 function Repuestos() {
     return (
 
         <div class="container">
             Modulo repuestos
+            <div class="container-button">
+                <button type="button" class="btn btn-info">Agregar repuestos</button>
+                <button type="button" class="btn btn-info">Editar repuestos</button>
+            </div>
+
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Proveedor</th>
+                        <th scope="col">Marca</th>
+                        <th scope="col">Modelo</th>
+                        <th scope="col">Item</th>
+                        <th scope="col">Referencia</th>
+                        <th scope="col">Cantidad</th>
+                        <th scope="col">Precio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                    </tr>
+                    <tr>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                    </tr>
+                    <tr>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }

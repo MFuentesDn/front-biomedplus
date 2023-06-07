67cad0473ecc36eac49b9a30aa50edccfae0c812
MFuentesDn <dayana.fuentesduran@gmail.com>
2023-06-07 06:42:42 -0500

    "alertas agregadas"

--- src/pages/alertas/alertas.js
+++ src/pages/alertas/alertas.js
@@ -52,6 +52,20 @@ function Alertas() {
                 </div>
               </div>
             </a>
+            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
+              <div class="row">
+                <div class="col-1 col-calendar1">
+                  07 JUN
+                </div>
+                <div class="col col-content">
+                  <div class="d-flex w-100 p-1 justify-content-between">
+                    <h5 class="mb-1">Serie: 1FSG34</h5>
+                  </div>
+                  <p class="mb-1">Apoyo</p>
+                  <small>Atril con rueda suelta, se requiere remplazo</small>
+                </div>
+              </div>
+            </a>
           </div>
         </div>
       </div>
